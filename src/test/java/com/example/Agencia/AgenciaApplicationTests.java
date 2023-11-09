0fbf0df84814847e17d7dbb3be008f5179e19993
Hugo <hcmr@cesar.school>
2023-11-09 17:55:07 -0300

    First Commit

---
+++ src/test/java/com/example/Agencia/AgenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Agencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
