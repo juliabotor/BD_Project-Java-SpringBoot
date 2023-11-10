587f7e5966d096c90226dedfbd49b0b7e3a147ef
Júlia <94140660+juliabotor@users.noreply.github.com>
2023-11-10 15:02:00 -0300

    Client implemantation

---
+++ src/main/java/com/example/Agencia/controller/ClientController.java
@@ -0,0 +1,50 @@
+package com.example.Agencia.controller;
+
+import com.example.Agencia.Client.Client;
+import com.example.Agencia.Client.ClientRepository;
+import com.example.Agencia.Client.ClientRequestDTO;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("client")
+public class ClientController {
+
+    @Autowired
+    private ClientRepository repository;
+
+    @GetMapping
+    public List<Client> getAllClient() {
+        List<Client> clients = repository.findAll();
+        return clients;
+    }
+
+
+    @PostMapping
+    public Client createClient(@RequestBody ClientRequestDTO clientData) {
+        Client client = new Client(clientData);
+        repository.save(client);
+        return client;
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity<Client> updateClient(@RequestBody ClientRequestDTO clientData) {
+        Optional<Client> optionalClient = repository.findById(clientData.cpf());
+        if (optionalClient.isPresent()) {
+            Client client = optionalClient.get();
+            client.setName(clientData.name());
+            client.setEmail(clientData.email());
+            client.setCpf(clientData.cpf());
+            repository.save(client);
+            return ResponseEntity.ok(client);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
