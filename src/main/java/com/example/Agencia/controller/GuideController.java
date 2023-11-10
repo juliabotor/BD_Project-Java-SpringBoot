c347f64a1c11138d9e366d59e944e3e7067ba157
Hugo <hcmr@cesar.school>
2023-11-10 16:58:44 -0300

    Guide implementation

--- src/main/java/com/example/Agencia/controller/GuideController.java
+++ src/main/java/com/example/Agencia/controller/GuideController.java
@@ -2,11 +2,17 @@ package com.example.Agencia.controller;
 
 import com.example.Agencia.Guide.Guide;
 import com.example.Agencia.Guide.GuideRepository;
+import com.example.Agencia.Guide.GuideRequestDTO;
+import com.example.Agencia.Package.Package;
+import com.example.Agencia.Package.PackageRequestDTO;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("guide")
 public class GuideController {
@@ -18,6 +24,26 @@ public class GuideController {
     public List<Guide> getAll(){
         List<Guide> guideList = repository.findAll();
         return guideList;
+    }
+
+    @PostMapping
+    public void saveGuide(@RequestBody GuideRequestDTO data){
+        Guide guideData = new Guide(data);
+        repository.save(guideData);
+        return;
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity updateGuide(@RequestBody GuideRequestDTO data){
+        Optional<Guide> optionalGuide = repository.findById(data.id());
+        if (optionalGuide.isPresent()){
+            Guide guide = optionalGuide.get();
+            guide.setDescription(data.description());
+            return ResponseEntity.ok(guide);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
